f27bd963a340fc72bba7265a40fafd2ece7355ca
Arkserks <symbiote96@gmail.com>
2025-10-13 10:31:07 +0500

    5.7 Знакомство с TCP/IP протоколами. Практика работы с TCP/IP протоколом

---
+++ tcp_users_client.py
@@ -0,0 +1,13 @@
+import socket
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('localhost', 12345))
+
+message = input("Введите сообщение для сервера: ")
+client_socket.send(message.encode())
+
+response = client_socket.recv(4096).decode()  # одно чтение ответа
+print("Ответ от сервера:")
+print(response)
+
+client_socket.close()
