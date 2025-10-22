01ac18f6c8795db024a06eeffcf7301751bb8075
Arkserks <symbiote96@gmail.com>
2025-10-22 17:33:44 +0500

    6.3 Знакомство с API клиентами. Практика: Написание API клиента

---
+++ clients/users/public_users_client.py
@@ -0,0 +1,27 @@
+from typing import TypedDict
+
+from httpx import Response
+
+from clients.api_client import APIClient
+
+class CreateUserDict(TypedDict):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
+    email: str
+    password: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+class PublicUsersClient(APIClient):
+    """
+    Клиент для работы с  /api/v1/users
+    """
+    def create_user_api(self, request: CreateUserDict) -> Response:
+        """
+        Метод выполняет создание нового пользователя
+        :param request: Словарь с email, password, lastName, firstName, middleName
+        :return: Объект Response с данными ответа.
+        """
+        return self.client.post("/api/v1/users", json=request)
---
+++ httpx_client.py
@@ -0,0 +1,26 @@
+import httpx
+
+
+# Проходим аутентификацию
+login_payload = {
+    "email": "aleks@example.com",
+    "password": "first"
+}
+login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+print('Login data:', login_response_data)
+
+
+# Инициализируем клиент с авторизацией
+client = httpx.Client(
+    base_url="http://localhost:8000",
+    timeout=100, # Таймаут в секундах
+    headers={"Authorization": f"Bearer {login_response_data['token']['accessToken']}"}
+)
+
+# Выполняем Get-запрос, с авторизацией
+get_user_me_response = client.get("/api/v1/users/me")
+get_user_me_response_data = get_user_me_response.json()
+
+# Выводим ответ в консоль
+print('Get user me data:', get_user_me_response_data)
